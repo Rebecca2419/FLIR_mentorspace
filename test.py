93cd9880525a9dad3229055c9250bceff655199c
yire <yire@kth.se>
2025-11-15 14:59:39 +0100

    refactor: use object-oriented programming

--- test.py
+++ test.py
@@ -1,64 +1,107 @@
-import cv2, numpy as np
+import cv2
 
-cam = 1
-min_area_ratio = 0.003
-border = 12
-blur = (15,15)
-thershold = 32
 
-cap = cv2.VideoCapture(cam)
-if not cap.isOpened():
-    raise SystemExit("Unable to open designated camera.")
+class DetectCamera:
 
-base = None
-print("Press 'b' to update background.")
-while True:
-    _, frame = cap.read()
-    cv2.imshow("background", frame)
-    if cv2.waitKey(1) & 0xFF == ord('b'):
-        base = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), blur, 0)
-        cv2.destroyWindow("background")
-        break
+    def __init__(self, camIndex: int, minAreaRatio=0.003, border=12, blur=(15,15), threshold=32):
+        self.camIndex = camIndex
+        self.minAreaRatio = minAreaRatio
+        self.border = border
+        self.blur = blur
+        self.threshold = threshold
 
-print("Press 'q' to quit; press 'b' to update background.")
-while True:
-    ok, frame = cap.read()
-    if not ok: break
+    def update_background(self):
+        self.cap = cv2.VideoCapture(self.camIndex)
+        if not self.cap.isOpened():
+            raise SystemExit("Unable to open designated camera.")
 
-    g = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), blur, 0)
-    diff = cv2.absdiff(g, base)
-    _, mask = cv2.threshold(diff, thershold, 255, cv2.THRESH_BINARY)
+        while True:
+            ok, frame = self.cap.read()
+            if not ok:
+                raise SystemExit("Unable to capture new frame.")
+            tempFrame = frame.copy()
+            cv2.putText(tempFrame, "Press ENTER to update background", (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255), 2)
+            cv2.imshow("background"+str(self.camIndex), tempFrame)
+            if cv2.waitKey(1) & 0xFF == 13:
+                self.base = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), self.blur, 0)
+                cv2.destroyWindow("background"+str(self.camIndex))
+                break
+    
+    """
+    return value: frame, result
+      frame     : original frame
+      result    : [(ROI, massCenter, areaSize), ...]
+        ROI       : (x, y, bw, bh)
+        massCenter: (x,y) or None
+        areaSize  : int or None
+    """
+    def detect(self):
+        ok, frame = self.cap.read()
+        if not ok:
+            raise SystemExit("Unable to capture new frame.")
 
-    k1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-    k2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11))
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, k1, iterations=2)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, k2, iterations=1)
+        g = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), self.blur, 0)
+        diff = cv2.absdiff(g, self.base)
+        _, mask = cv2.threshold(diff, self.threshold, 255, cv2.THRESH_BINARY)
 
-    h, w = mask.shape
-    mask[:border,:] = mask[-border:,:] = 0
-    mask[:,:border] = mask[:,-border:] = 0
+        k1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+        k2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, k1, iterations=2)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, k2, iterations=1)
 
-    min_area = int(min_area_ratio * w * h)
-    cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for c in cnts:
-        if cv2.contourArea(c) < min_area:
-            continue
-        x,y,bw,bh = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x,y), (x+bw,y+bh), (0,255,0), 2)
+        h, w = mask.shape
+        mask[:self.border,:] = mask[-self.border:,:] = 0
+        mask[:,:self.border] = mask[:,-self.border:] = 0
 
-        roi = mask[y:y+h, x:x+w]
-        M = cv2.moments(roi, binaryImage=True)
-        if M["m00"] > 0:
-            cx = int(M["m10"] / M["m00"])
-            cy = int(M["m01"] / M["m00"])
-            cv2.circle(frame, (x+cx, y+cy), 5, (0,0,255), -1)
+        min_area = int(self.minAreaRatio * w * h)
+        cnts, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        result = []
+        for c in cnts:
+            if cv2.contourArea(c) < min_area:
+                continue
+            x, y, bw, bh = cv2.boundingRect(c)
+            ROI = (x, y, bw, bh)
+            roi = mask[y:y+bh, x:x+bw]
 
-    cv2.imshow("Detection", frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
-    if key == ord('b'):
-        base = g.copy()
+            M = cv2.moments(roi, binaryImage=True)
+            if M["m00"] > 0:
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
+                massCenter = (x+cx, y+cy)
+                areaSize = int(M["m00"])
+            else:
+                massCenter = None
+                areaSize = None
+            
+            result.append((ROI, massCenter, areaSize))    
 
-cap.release()
-cv2.destroyAllWindows()
+        return frame, result
+    
+    def release(self):
+        self.cap.release()
+
+
+if __name__ == "__main__":
+    cam1 = DetectCamera(1)
+    cam1.update_background()
+
+    while True:
+        frame, result = cam1.detect()
+
+        cv2.putText(frame, "Press B to update background", (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255), 2)
+        cv2.putText(frame, "Press Q to quit", (10,60), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255), 2)
+        for ROI, massCenter, areaSize in result:
+            cv2.rectangle(frame, (ROI[0],ROI[1]), (ROI[0] + ROI[2], ROI[1] + ROI[3]), (0,255,0), 2)
+            if massCenter != None:
+                cv2.circle(frame, massCenter, 5, (0,0,255), -1)
+                cv2.putText(frame, str(areaSize), (ROI[0], ROI[1]+ROI[3]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255))
+        cv2.imshow("detection", frame)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+        if key == ord('b'):
+            cam1.update_background()
+
+    cam1.release()
